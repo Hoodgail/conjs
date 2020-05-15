a202ade8c5fa4d7fb0bb05bcfba0f8e845248bde
Hoodgail <45743294+Hoodgail@users.noreply.github.com>
2020-05-15 18:36:54 -0400

    Update con.module.js

--- build/con.module.js
+++ build/con.module.js
@@ -1,6 +1,15 @@
 var con = (function () {
-  var con = {};
+  window.con = {};
   con.json = {};
+  con.others = {};
+  con.style = {};
+  con.style.button = `background: #0000004a;
+    padding: 9px;
+    color: white;
+    font-size: 10px;
+    border-radius: 5px;
+    border: solid 1px #ffffff61;
+    width:-webkit-fill-available;`;
   con.prefix = ">";
   con.body = document.createElement("div");
   con.input = document.createElement("input");
@@ -93,21 +102,18 @@ var con = (function () {
   con.col = function (msg, col) {
     return `<span style="color:${col ? col : "white"};">${msg}</span>`;
   };
-  con.log = function (msg, msg2, conf) {
+  con.log = function (msg, conf) {
     var div = document.createElement("div");
     div.innerHTML = "";
     div.style = `margin-bottom:5px;margin-top:5px;`;
     if (conf) if (conf.back) div.style.background = conf.back;
     if (typeof msg === "string") div.innerHTML += msg ? msg : "";
-    if (typeof msg2 === "string") div.innerHTML += msg2 ? msg2 : "";
     if (typeof msg === "object")
-      div.innerHTML += con.pre(con.stringify(msg ? msg : ""));
-    if (typeof msg2 === "object")
       div.innerHTML += con.pre(con.stringify(msg ? msg : ""));
     con.element.appendChild(div);
   };
   con.err = function (msg, msg2, conf) {
-    con.log(msg, false, {
+    con.log(msg, {
       back: "red",
     });
   };
@@ -121,10 +127,90 @@ var con = (function () {
       con.body.appendChild(con.input);
     }
   };
+  con.addBtn = function (name, callback) {
+    var btn = document.createElement("button");
+    btn.innerHTML = name;
+    btn.style = con.style.button;
+    btn.addEventListener("click", () => {
+      try {
+        callback({
+          time: Date.now(),
+          con: con,
+        });
+      } catch (r) {
+        con.err(r);
+        console.log(r);
+      }
+    });
+    con.element.appendChild(btn);
+  };
+  con.addInput = function (name, callback, to) {
+    var div = document.createElement("div");
+    var input = document.createElement("input");
+    div.appendChild(input);
+    var btn = document.createElement("button");
+    div.appendChild(btn);
+    btn.innerHTML = name;
+    btn.style = con.style.button;
+    btn.style.width = "auto";
+    btn.style.marginLeft = "10px";
+    input.style = con.style.button;
+    input.style.width = "auto";
+    input.placeholder = "Input";
+    btn.addEventListener("click", () => {
+      try {
+        callback({
+          time: Date.now(),
+          con: con,
+          value: input.value,
+          input: input,
+        });
+      } catch (r) {
+        con.err(r);
+        console.log(r);
+      }
+    });
+    if (!to) {
+      con.element.appendChild(div);
+    } else {
+      to.appendChild(div);
+    }
+  };
+  con.others.loadJS = function (src, onload) {
+    if (Array.isArray(src)) {
+      var length = src.length;
+      var loaded = 0;
+      for (js of src) {
+        var el = document.createElement("script");
+        el.src = js;
+        el.onload = function () {
+          loaded += 1;
+          load(loaded);
+        };
+        document.body.appendChild(el);
+      }
+      function load(v) {
+        if (v === length) {
+          onload();
+        }
+      }
+    } else {
+      var el = document.createElement("script");
+      el.src = src;
+      el.onload = function () {
+        onload();
+      };
+      document.body.appendChild(el);
+    }
+  };
+  con.others.sec = function (s) {
+    return s * 1000;
+  };
   con.input.addEventListener("keypress", function (e) {
     if (e.keyCode === 13)
       try {
-        eval(this.value);
+        var code = eval(this.value);
+        console.log(code);
         this.value = "";
       } catch (e) {
         con.err(String(e));
